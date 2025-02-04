6c29b269456c7f0b160e59fab153dc0a92478faf
Imnotcoderdude <topsolo7@naver.com>
2025-02-04 13:28:53 +0900

    feat : 회원 기능 구현

---
+++ src/main/java/com/sparta/myselectshop2/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sparta.myselectshop2.security;
+
+import com.sparta.myselectshop2.entity.User;
+import com.sparta.myselectshop2.repository.UserRepository;
+import com.sparta.myselectshop2.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
