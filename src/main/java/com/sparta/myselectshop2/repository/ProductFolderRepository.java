51cbcfbe604c84a4baac163d5242ebb70bc287c0
Imnotcoderdude <topsolo7@naver.com>
2025-02-04 15:37:59 +0900

    feat : 상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop2/repository/ProductFolderRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.myselectshop2.repository;
+
+import com.sparta.myselectshop2.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+
+}
